5b06f82132a20fe946cbddcf69dc4b37929f5df1
Trương Minh Phát <tmphat1312@gmail.com>
2024-01-25 10:26:55 +0700

    feat: Top categories

--- src/primary-server/routes/view.route.ts
+++ src/primary-server/routes/view.route.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render("pages/home", { title: "Home" });
+  res.render("pages/home", { title: "Fohoso - Hiệu sách của bạn" });
 });
 
 export default router;
