4f0b6b7803fb13256196ff58c8100860f66ebb95
Trương Minh Phát <tmphat1312@gmail.com>
2024-01-25 11:52:25 +0700

    feat: Top 5 high rated books

--- src/primary-server/routes/view.route.ts
+++ src/primary-server/routes/view.route.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
+import Book from "../models/book.model";
 
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.render("pages/home", { title: "Fohoso - Hiệu sách của bạn" });
+router.get("/", async (req, res) => {
+  const [top5HighRatedBooks, top5RecentlyAddedBooks] = await Promise.all([
+    Book.find({}).sort({ rating: -1 }).limit(5).lean(),
+    Book.find({}).sort({ createdAt: -1 }).limit(5).lean(),
+  ]);
+
+  res.render("pages/home", {
+    title: "Fohoso - Hiệu sách của bạn",
+    top5HighRatedBooks,
+    top5RecentlyAddedBooks,
+  });
 });
 
 export default router;
