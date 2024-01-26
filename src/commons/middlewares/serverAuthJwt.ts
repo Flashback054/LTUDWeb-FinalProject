54417577059d0ba8d30c1f09741459fab2247f00
21120502 <37113714+Flashback054@users.noreply.github.com>
2024-01-27 01:53:52 +0700

    feat: Use JWT to authorize communications between servers

---
+++ src/commons/middlewares/serverAuthJwt.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { CustomRequestError } from "../utils/AppError";
+
+const serverAuthSecret = process.env.SERVER_AUTH_SECRET;
+const serverAuthPayload = process.env.SERVER_AUTH_PAYLOAD;
+
+export const checkAndValidateJwt = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		let token = req.headers.authorization as string;
+		if (token) {
+			const decoded: any = jwt.verify(token, serverAuthSecret);
+			if (decoded && decoded.name === serverAuthPayload) {
+				// If the jwt is valid, then check return payload
+				token = jwt.sign(
+					{
+						name: serverAuthPayload,
+					},
+					serverAuthSecret,
+					{
+						expiresIn: "1m",
+					}
+				);
+
+				res.set("Authorization", token);
+				return next();
+			}
+		}
+
+		throw new CustomRequestError(401, {
+			reasonPhrase: "UNAUTHORIZED",
+			message: "Not authorized server",
+		});
+	} catch (err) {
+		next(err);
+	}
+};
