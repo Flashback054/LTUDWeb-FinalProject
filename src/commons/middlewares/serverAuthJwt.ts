b1fe7a3a1bf2e44e0f0c4ab11f447dc16cf491e6
21120502 <37113714+Flashback054@users.noreply.github.com>
2024-01-27 13:09:45 +0700

    fix: req.request on views route

--- src/commons/middlewares/serverAuthJwt.ts
+++ src/commons/middlewares/serverAuthJwt.ts
@@ -1,11 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { CustomRequestError } from "../utils/AppError";
+import { optional } from "zod";
 
 const serverAuthSecret = process.env.SERVER_AUTH_SECRET;
 const serverAuthPayload = process.env.SERVER_AUTH_PAYLOAD;
 
-export const checkAndValidateJwt = async (
+export const requireValidateJWT = async (
 	req: Request,
 	res: Response,
 	next: NextFunction
@@ -35,6 +36,43 @@ export const checkAndValidateJwt = async (
 			reasonPhrase: "UNAUTHORIZED",
 			message: "Not authorized server",
 		});
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const optionalValidateJWT = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	try {
+		let token = req.headers.authorization as string;
+		if (token) {
+			const decoded: any = jwt.verify(token, serverAuthSecret);
+			if (decoded && decoded.name === serverAuthPayload) {
+				// If the jwt is valid, then check return payload
+				token = jwt.sign(
+					{
+						name: serverAuthPayload,
+					},
+					serverAuthSecret,
+					{
+						expiresIn: "1m",
+					}
+				);
+
+				res.set("Authorization", token);
+				return next();
+			}
+
+			throw new CustomRequestError(401, {
+				reasonPhrase: "UNAUTHORIZED",
+				message: "Not authorized server",
+			});
+		}
+
+		next();
 	} catch (err) {
 		next(err);
 	}
