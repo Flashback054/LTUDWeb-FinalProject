9bb531c7482b2853df799943056d21edfadb278c
21120502 <37113714+Flashback054@users.noreply.github.com>
2024-01-28 18:37:35 +0700

    change: Use transaction for payment

--- src/payment-server/controllers/payment.controller.ts
+++ src/payment-server/controllers/payment.controller.ts
@@ -2,61 +2,70 @@ import Payment from "../models/payment.model";
 import PaymentAccount from "../models/paymentAccount.model";
 import ControllerFactory from "../../commons/controllers/controller.factory";
 import { Request, Response, NextFunction } from "express";
-import { optional } from "zod";
 import AppError from "../../commons/utils/AppError";
+import mongoose from "mongoose";
 
 export const getAllPayments = ControllerFactory.getAll(Payment, {
-  allowNestedQueries: ["user"],
-  populate: {
-    path: "order",
-    populate: [
-      {
-        path: "user",
-        select: "name email",
-      },
-      {
-        path: "orderDetails.book",
-        select: "name",
-      },
-    ],
-  },
+	allowNestedQueries: ["user"],
+	populate: {
+		path: "order",
+		populate: [
+			{
+				path: "user",
+				select: "name email",
+			},
+			{
+				path: "orderDetails.book",
+				select: "name",
+			},
+		],
+	},
 });
 export const getPayment = ControllerFactory.getOne(Payment, {
-  populate: {
-    path: "order",
-    populate: {
-      path: "user",
-      select: "name email",
-    },
-  },
+	populate: {
+		path: "order",
+		populate: {
+			path: "user",
+			select: "name email",
+		},
+	},
 });
 export const createPayment = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
+	req: Request,
+	res: Response,
+	next: NextFunction
 ) => {
-  try {
-    const { finalPrice, user } = req.body;
-    const userAccount = await PaymentAccount.findOne({ user });
-    const adminAccount = await PaymentAccount.findOne({
-      isAdminAccount: true,
-    });
+	const session = await mongoose.startSession();
 
-    if (userAccount.balance >= finalPrice) {
-      const newPayment = await Payment.create(req.body);
-      userAccount.balance -= newPayment.finalPrice;
-      adminAccount.balance += newPayment.finalPrice;
+	try {
+		session.startTransaction();
 
-      await userAccount.save();
-      await adminAccount.save();
+		const { finalPrice, user } = req.body;
+		const userAccount = await PaymentAccount.findOne({ user });
+		const adminAccount = await PaymentAccount.findOne({
+			isAdminAccount: true,
+		});
 
-      res.created(newPayment);
-    } else {
-      throw new AppError(400, "BAD_REQUEST", "Số dư không đủ");
-    }
-  } catch (err) {
-    throw err;
-  }
+		if (userAccount.balance >= finalPrice) {
+			const newPayment = await Payment.create(req.body);
+			userAccount.balance -= newPayment.finalPrice;
+			adminAccount.balance += newPayment.finalPrice;
+
+			await userAccount.save({ session });
+			await adminAccount.save({ session });
+
+			await session.commitTransaction();
+			session.endSession();
+
+			res.created(newPayment);
+		} else {
+			throw new AppError(400, "BAD_REQUEST", "Số dư không đủ");
+		}
+	} catch (error) {
+		await session.abortTransaction();
+		session.endSession();
+		throw error;
+	}
 };
 export const updatePayment = ControllerFactory.updateOne(Payment);
 export const deletePayment = ControllerFactory.deleteOne(Payment);
